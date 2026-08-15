a910417d69ee27d2604fb4672abe6d7b6c57b7bc
quansu268t <nguyenthingocbich268qst@gmail.com>
2026-08-15 14:22:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -302,7 +302,18 @@ app.get("/api/monetag-postback", async (req, res) => {
 
             return res.status(500).send("Server configuration error");
         }
-
+        console.log("[Monetag] Key received:", !!key);
+        console.log("[Monetag] Secret configured:", !!expectedSecret);
+        console.log(
+         "[Monetag] Key length:",
+         typeof key === "string" ? key.length : 0
+        );
+        console.log(
+          "[Monetag] Secret length:",
+          typeof expectedSecret === "string"
+             ? expectedSecret.length
+             : 0
+        );
         if (
             typeof key !== "string" ||
             key !== expectedSecret
